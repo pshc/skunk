40490f720d7ea54cb4bd9b725e4894979b816863
Paul Collier <paul@paulcollier.ca>
2021-09-27 15:26:07 -0700

    /cookie multiplier with items

--- commands/cookie.ts
+++ commands/cookie.ts
@@ -1,7 +1,7 @@
 import { randomInt } from 'crypto';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import type { CommandInteraction } from 'discord.js';
-import { lookupArena } from '../api';
+import { ItemId, lookupArena } from '../api';
 import { updateHighScore } from './score';
 
 export const data: SlashCommandBuilder = new SlashCommandBuilder()
@@ -27,9 +27,54 @@ export async function execute(interaction: CommandInteraction) {
   // roll and add 'em
   const a = randomInt(6) + 1;
   const b = randomInt(6) + 1;
+  const roll = BigInt(a + b);
+  let outcome = `Rolled ${a} + ${b}.`;
+  let totalAward = roll;
+
+  // apply inventory bonuses
+  const inventory = `${arena}:inventory:${playerId}`;
+  const itemIds: ItemId[] = await redis.smembers(inventory);
+  if (itemIds.length > 0) {
+    // initial multiplier derived from difficulty class
+    let exponent = randomInt(6);
+    const difficulty = (exponent + 1) * 5;
+    outcome += ` Bonus DC ${difficulty} adds +${exponent} multiplier.`;
+    // each item has a chance to add to the multiplier
+    let anySuccess = false;
+    for (const itemId of itemIds) {
+      const itemName = await redis.get(`${arena}:item:${itemId}:name`);
+      const strength = Number(await redis.hget(`${arena}:item:${itemId}:stats`, 'STR'));
+      const skillCheck = randomInt(20) + 1;
+      if (skillCheck === 20) {
+        exponent += 2;
+        outcome += ` ${itemName} nat 20 +2!`;
+        anySuccess = true;
+      } else if (skillCheck === 1) {
+        exponent -= 1;
+        outcome += ` ${itemName} crit fail -1.`;
+      } else if (skillCheck + strength >= difficulty) {
+        exponent += 1;
+        outcome += ` ${itemName} passed +1 (${skillCheck} + STR ${strength}).`;
+        anySuccess = true;
+      }
+    }
+    // did we pass any difficulty checks?
+    if (anySuccess) {
+      exponent = Math.max(0, exponent);
+      const multiplier = BigInt(2) ** BigInt(exponent);
+      outcome += ` Multiplier 2^${exponent} = ${multiplier.toLocaleString()}.`;
+      totalAward *= multiplier;
+    } else {
+      outcome += ` Multiplier failed.`;
+    }
+    outcome += ` Total ${totalAward.toLocaleString()}.`;
+  }
+
   const oldScore = BigInt(await redis.hget(`${arena}:scores`, playerId));
-  const newScore = oldScore + BigInt(a + b);
+  const newScore = oldScore + BigInt(totalAward);
+  outcome += ` Your new score is ${newScore.toLocaleString()}.`;
+
   await redis.hset(`${arena}:scores`, playerId, newScore.toString());
   await updateHighScore(arena, playerId, newScore);
-  await interaction.reply(`Rolled ${a} + ${b}. Your new score is ${newScore.toLocaleString()}.`);
+  await interaction.reply(outcome);
 }
