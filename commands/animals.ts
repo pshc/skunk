a188bfb5a35817d43b13b79ed1a366760f54797b
Paul Collier <paul@paulcollier.ca>
2021-09-28 14:35:37 -0700

    /animals shows player counts

--- commands/animals.ts
+++ commands/animals.ts
@@ -11,17 +11,30 @@ export async function execute(interaction: CommandInteraction) {
   const arena = lookupArena(interaction);
   await lookupPlayerId(arena, interaction);
 
-  const abandonedIds: ItemId[] = await redis.smembers(`${arena}:abandoned_items`);
-  if (!abandonedIds.length) {
-    await interaction.reply({ content: 'The streets are empty.', ephemeral: true });
-    return;
+  // count each players' pets
+  const players = await redis.hgetall(`${arena}:names`);
+  const petCounts: [string, number][] = [];
+  for (const playerId in players) {
+    const playerName = players[playerId];
+    const inventorySize = Number(await redis.scard(`${arena}:inventory:${playerId}`));
+    petCounts.push([playerName, inventorySize]);
   }
+  petCounts.sort(([nameA, countA], [nameB, countB]) => countA - countB);
+  const lines = petCounts.map(([name, count]) => `${name}: ${count}`);
+  const playersDesc = '```\n' + lines.join('\n') + '\n```';
 
   // scan for abandoned animals
+  const abandonedIds: ItemId[] = await redis.smembers(`${arena}:abandoned_items`);
   const descs = await Promise.all(abandonedIds.map(async (itemId: ItemId) => {
     const itemName = await redis.get(`${arena}:item:${itemId}:name`);
     return itemName;
   }));
+  let streetDesc;
+  if (abandonedIds.length) {
+    streetDesc = `On the streets: ${descs.join(', ')}`;
+  } else {
+    streetDesc = 'The streets are empty.';
+  }
 
-  await interaction.reply(`You see on the streets: ${descs.join(', ')}`);
+  await interaction.reply(playersDesc + '\n' + streetDesc);
 }
