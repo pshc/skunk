57412479507cd21c6fd203f1c6ffbc9060ab3847
Paul Collier <paul@paulcollier.ca>
2021-09-27 20:29:08 -0700

    Configurable cookie difficulty

--- commands/cookie.ts
+++ commands/cookie.ts
@@ -8,6 +8,22 @@ export const data: SlashCommandBuilder = new SlashCommandBuilder()
     .setName('cookie')
     .setDescription('Click for a cookie.');
 
+data.addStringOption(option =>
+  option.setName('difficulty')
+    .setDescription('Set the difficulty level of your bonus skill checks.')
+    .setRequired(false)
+    .addChoices([
+      ["random", "random"],
+      ["very easy", "very easy"], ["easy", "easy"], ["medium", "medium"],
+      ["hard", "hard"], ["very hard", "very hard"], ["nearly impossible", "nearly impossible"]
+    ])
+);
+
+const DIFFICULTIES = {
+  'very easy': 0, 'easy': 1, 'medium': 2,
+  'hard': 3, 'very hard': 4, 'nearly impossible': 5,
+};
+
 export async function execute(interaction: CommandInteraction) {
   const { redis } = global as any;
   const arena = lookupArena(interaction);
@@ -15,6 +31,11 @@ export async function execute(interaction: CommandInteraction) {
   if (!playerId) {
     throw new Error('Not playing');
   }
+  const difficultyName = interaction.options.getString('difficulty');
+  // the exponent will increase later and be used to calculate the multiplier
+  let exponent: number = difficultyName === 'random' ? randomInt(6) : (DIFFICULTIES as any)[difficultyName];
+  exponent = Math.max(0, Math.min(5, exponent));
+  const difficulty = Math.max(5, Math.min(30, (exponent + 1) * 5));
 
   const cooldownKey = `${arena}:cookie_cooldown:${playerId}`;
   const didSet = await redis.set(cooldownKey, '1', 'NX', 'EX', '20');
@@ -28,7 +49,7 @@ export async function execute(interaction: CommandInteraction) {
   const a = randomInt(6) + 1;
   const b = randomInt(6) + 1;
   const roll = BigInt(a + b);
-  let outcome = `Rolled ${a} + ${b}.`;
+  const outcome = [`  ${a} + ${b}.`];
   let totalAward = roll;
 
   // apply inventory bonuses
@@ -36,9 +57,7 @@ export async function execute(interaction: CommandInteraction) {
   const itemIds: ItemId[] = await redis.smembers(inventory);
   if (itemIds.length > 0) {
     // initial multiplier derived from difficulty class
-    let exponent = randomInt(6);
-    const difficulty = (exponent + 1) * 5;
-    outcome += ` Bonus DC ${difficulty} adds +${exponent} multiplier.`;
+    outcome.push(`+ ${exponent} ${difficulty}`);
     // each item has a chance to add to the multiplier
     let anySuccess = false;
     for (const itemId of itemIds) {
@@ -47,36 +66,36 @@ export async function execute(interaction: CommandInteraction) {
       const skillCheck = randomInt(20) + 1;
       if (skillCheck === 20) {
         exponent += 2;
-        outcome += ` ${itemName} nat 20 +2!`;
+        outcome.push(`+ 2 ${itemName} nat 20`);
         anySuccess = true;
       } else if (skillCheck === 1) {
         exponent -= 1;
-        outcome += ` ${itemName} crit fail -1.`;
+        outcome.push(`- 1 ${itemName} crit fail`);
       } else if (skillCheck + strength >= difficulty) {
         exponent += 1;
-        outcome += ` ${itemName} +1.`;
+        outcome.push(`+ 1 ${itemName}`);
         anySuccess = true;
       } else {
-        outcome += ` ${itemName} 0.`;
+        outcome.push(`  0 ${itemName}`);
       }
     }
     // did we pass any difficulty checks?
     if (anySuccess) {
       exponent = Math.max(0, exponent);
       const multiplier = BigInt(2) ** BigInt(exponent);
-      outcome += ` Multiplier 2^${exponent} = ${multiplier.toLocaleString()}.`;
+      outcome.push(`+ 2^${exponent} = ${multiplier.toLocaleString()}x`);
       totalAward *= multiplier;
     } else {
-      outcome += ` Multiplier failed.`;
+      outcome.push(`- multiplier failed`);
     }
-    outcome += ` Total **${totalAward.toLocaleString()}**.`;
+    outcome.push(`+ total bonus ${totalAward.toLocaleString()}`);
   }
 
   const oldScore = BigInt(await redis.hget(`${arena}:scores`, playerId));
   const newScore = oldScore + BigInt(totalAward);
-  outcome += ` Your new score is ${newScore.toLocaleString()}.`;
+  outcome.push(`  score ${newScore.toLocaleString()}`);
 
   await redis.hset(`${arena}:scores`, playerId, newScore.toString());
   await updateHighScore(arena, playerId, newScore);
-  await interaction.reply(outcome);
+  await interaction.reply('```diff\n' + outcome.join('\n') + '\n```');
 }
