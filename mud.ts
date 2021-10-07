69cc6a5383a13d87bef18da6091ed64c7d97d528
Paul Collier <paul@paulcollier.ca>
2021-10-07 13:51:36 -0700

    Pos, Direction

--- mud.ts
+++ mud.ts
@@ -1,5 +1,44 @@
+/// A 3D coordinate in space.
+export type Pos = { x: number, y: number, z: number };
+export const SPAWN = '0,0,0';
+
+/// A cardinal direction or up/down.
+export type Direction = 'n' | 's' | 'e' | 'w' | 'u' | 'd';
+export const DIRECTIONS: Direction[] = ['n', 's', 'e', 'w', 'u', 'd'];
+
 /// Clean up some player-input string.
 export function sanify(desc: string): string {
   const pruned = desc.trim().replace(/[^\w\s,.'";:()<>!?&$%#/+=~-]/g, '');
   return pruned.replace(/\s+/g, ' ').trim().slice(0, 300);
+}
+
+/// Converts a redis str to a 3D coordinate object.
+export function strToPos(str: string): Pos {
+  const match = /^(-?\d+),(-?\d+),(-?\d+)$/.exec(str);
+  if (!match) {
+    throw new Error(`Invalid pos \`${sanify(str)}\``);
+  }
+  return {x: Number(match[1]), y: Number(match[2]), z: Number(match[3])};
+}
+
+/// Converts a 3D coordinate to a redis str.
+export function posToStr(pos: Pos): string {
+  if (!('x' in pos) || !('y' in pos) || !('z' in pos)) {
+    throw new Error(`Invalid Pos \`${sanify(pos.toString())}\``);
+  }
+  return `${pos.x},${pos.y},${pos.z}`;
+}
+
+/// Add a direction vector to a coordinate to yield a new coordinate.
+export function addDirection(pos: Pos, direction: Direction): Pos {
+  const { x, y, z } = pos;
+  switch (direction) {
+    case 'n': return { x, y: y - 1, z };
+    case 's': return { x, y: y + 1, z };
+    case 'e': return { x: x + 1, y, z };
+    case 'w': return { x: x - 1, y, z };
+    case 'u': return { x, y, z: z + 1 };
+    case 'd': return { x, y, z: z - 1 };
+    default: throw new Error(`Bad direction \`${sanify(direction)}\``);
+  }
 }
--- test/mud.test.ts
+++ test/mud.test.ts
@@ -1,7 +1,35 @@
 import test from 'ava';
-import { sanify } from '../mud';
+import { SPAWN, addDirection, sanify, strToPos, posToStr } from '../mud';
 
 test('sanify', t => {
   t.is(sanify(' **A** '), 'A');
   t.is(sanify('what @   ever'), 'what ever');
+});
+
+test('strToPos', t => {
+  t.deepEqual(strToPos(SPAWN), { x: 0, y: 0, z: 0 });
+  t.deepEqual(strToPos('0,-10,300'), { x: 0, y: -10, z: 300 });
+  t.throws(() => strToPos('1'));
+  t.throws(() => strToPos('1,2,'));
+  t.throws(() => strToPos('1,2,5,'));
+  t.throws(() => strToPos('1,2,5,10'));
+  t.throws(() => strToPos('1,,5'));
+  t.throws(() => strToPos('1,-,5'));
+  t.throws(() => strToPos('a,2,3'));
+});
+
+test('posToStr', t => {
+  t.is(posToStr({ x: 0, y: 0, z: 0 }), SPAWN);
+  t.is(posToStr({ x: 1, y: 2, z: -42 }), '1,2,-42');
+  t.throws(() => posToStr({} as any));
+});
+
+test('addDirection', t => {
+  const spawn = strToPos(SPAWN);
+  t.is(posToStr(addDirection(spawn, 'n')), '0,-1,0');
+  t.is(posToStr(addDirection(spawn, 's')), '0,1,0');
+  t.is(posToStr(addDirection(spawn, 'e')), '1,0,0');
+  t.is(posToStr(addDirection(spawn, 'w')), '-1,0,0');
+  t.is(posToStr(addDirection(spawn, 'u')), '0,0,1');
+  t.is(posToStr(addDirection(spawn, 'd')), '0,0,-1');
 });
